5a219e8865d07b2a1f4a7e63515d352a214c1612
Steve Sullivan <ssully28@gmail.com>
2019-02-24 17:19:03 -0800

    Now adds memes

--- meme.js
+++ meme.js
@@ -6,38 +6,31 @@ let url = document.getElementById("memeurl");
 
 let memeList = document.getElementById("memelist");
 
-/*
-<div class="row my-2">
-            <div class="col"></div>
-            <div class="col col-6 text-center d-flex flex-column justify-content-between meme" style="background-image: url(\'https://photos.smugmug.com/BW/i-sL6ZcWh/0/3a505840/X3/tahoe_BW-X3.jpg\')">
-                <div class="align-item-start memehead"><h3 class="display-3">Whatever!!!</h3></div>
-                <div class="align-item-end memefoot"><h3 class="display-5">Whatever memes normally say...</h3><i class="far fa-trash-alt trash"></i></div>
-            </div>
-            <div class="col"></div>
-        </div>
-*/
-
-let memeTemplate = `
-<div class="col"></div>
-<div class="col col-6 text-center d-flex flex-column justify-content-between meme" style="background-image: url(\'https://photos.smugmug.com/BW/i-sL6ZcWh/0/3a505840/X3/tahoe_BW-X3.jpg\')">
-    <div class="align-item-start memehead"><h3 class="display-3">Whatever!!!</h3></div>
-    <div class="align-item-end memefoot"><h3 class="display-5">Whatever memes normally say...</h3><i class="far fa-trash-alt trash"></i></div>
-</div>
-<div class="col"></div>
-</div>  
-`;
 
 function addMeme (){
-    
-    alert("Top Text : " + topText.value);
-
-    //let newMemeNode = document.createElement(memeTemplate);
-    //memeList.appendChild(newMemeNode);
-
-    document.getElementById('memeplaceholder').insertAdjacentHTML('afterend', memeTemplate);
-
+    document.getElementById('memeplaceholder').insertAdjacentHTML('afterend', createMeme());
 }
 
-function deleteMeme (){
+function createMeme(){
+    let meme_header = topText.value;
+    let meme_footer = bottomText.value;
+    let meme_url = url.value;
+
+    let memeTemplateV = `
+        <div class="row my-2" onclick="deleteMeme()">
+        <div class="col"></div>
+        <div class="col col-6 text-center d-flex flex-column justify-content-between meme" style="background-image: url('${meme_url}')">
+            <div class="align-item-start memehead"><h3 class="display-3">${meme_header}</h3></div>
+            <div class="align-item-end memefoot"><h3 class="display-5">${meme_footer}</h3><i class="far fa-trash-alt trash"></i></div>
+        </div>
+        <div class="col"></div>
+        </div>  
+    `;
+
+    return memeTemplateV;
+}
+
+function deleteMeme (obj){
+    
     alert("Delete dat meme!");
 }
