58897876cb51af8c0d411fee9ac26c20a21f1d0b
Steve Sullivan <ssully28@gmail.com>
2019-02-24 20:11:15 -0800

    Adding ability to delete memes

--- meme.js
+++ meme.js
@@ -1,23 +1,30 @@
 
-
+let meme_count = 0;
 let topText = document.getElementById("memehead");
 let bottomText = document.getElementById("memefoot");
 let url = document.getElementById("memeurl");
 
-let memeList = document.getElementById("memelist");
-
-
 function addMeme (){
+    // Insert the meme:
     document.getElementById('memeplaceholder').insertAdjacentHTML('afterend', createMeme());
+
+    // Use counter for ID for memes (make it easy to delete them later):
+    meme_count++;
+    
+    // Clear out the form:
+    topText.value = null;
+    bottomText.value = null;
+    url.value = null;
 }
 
 function createMeme(){
+    // Grab the values from the input boxes:
     let meme_header = topText.value;
     let meme_footer = bottomText.value;
     let meme_url = url.value;
 
     let memeTemplateV = `
-        <div class="row my-2" onclick="deleteMeme()">
+        <div id="${meme_count}" class="row my-2" onclick="deleteMeme(${meme_count})">
         <div class="col"></div>
         <div class="col col-6 text-center d-flex flex-column justify-content-between meme" style="background-image: url('${meme_url}')">
             <div class="align-item-start memehead"><h3 class="display-3">${meme_header}</h3></div>
@@ -30,7 +37,7 @@ function createMeme(){
     return memeTemplateV;
 }
 
-function deleteMeme (obj){
-    
-    alert("Delete dat meme!");
+function deleteMeme (meme_id){
+    let memeToDelete = document.getElementById(meme_id);
+    memeToDelete.remove();
 }
